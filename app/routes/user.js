6531f146734a9d52b32280c43931ec9c1e55a249
Monumentix <brian@monumentixwebdesign.com>
2017-07-20 13:57:16 -0400

    ADDED: new relic to keep server alive

--- app/routes/user.js
+++ app/routes/user.js
@@ -186,34 +186,4 @@ module.exports = function (app,passport){
 
 
 
-  app.get("/user/reset", function(req,res){
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // secure:true for port 465, secure:false for port 587
-        auth: {
-            user: process.env.EMAIL_NAME,
-            pass: process.env.EMAIL_PASS
-        }
-    });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-        to: 'brian@monumentixwebdesign.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world ?', // plain text body
-        html: '<b>Hello world ?</b>' // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
-    });
-  });//end /user/reset
-
 };//end module.exports
